c1a7e9dcfc8838b0ed341b3765d572691b0bc4c5
meganukebmp <kristiana2000@abv.bg>
2025-08-26 18:42:37 +0200

    [RedDevil V2] Restore matching I2C magic

--- Controllers/PowerColorGPUController/PowerColorGPUControllerDetect.cpp
+++ Controllers/PowerColorGPUController/PowerColorGPUControllerDetect.cpp
@@ -14,17 +14,23 @@
 #include "PowerColorRedDevilV2Controller/RGBController_PowerColorRedDevilV2.h"
 #include "PowerColorRedDevilV2Controller/PowerColorRedDevilV2Controller.h"
 
+#define RED_DEVIL_V1_MAGIC_ADDR 0x90
+#define RED_DEVIL_V2_MAGIC_ADDR 0x82
+
+static const unsigned char magic_v1[3] = {0x01, 0x05, 0x00};
+static const unsigned char magic_v2[3] = {0x01, 0x32, 0x00};
+
 void DetectPowerColorRedDevilGPUControllersV2(i2c_smbus_interface* bus, uint8_t i2c_addr, const std::string& name)
 {
-    //unsigned char data[3] = {0};
-    //int ret = bus->i2c_smbus_read_i2c_block_data(i2c_addr, 0x90, 3, data);
-    //if(ret == 3 && memcmp(data, indicator, 3) == 0)
-    //{
+    unsigned char data[3];
+    int ret = bus->i2c_smbus_read_i2c_block_data(i2c_addr, RED_DEVIL_V2_MAGIC_ADDR, 3, data);
+    if(ret == 3 && memcmp(data, magic_v2, 3) == 0)
+    {
         PowerColorRedDevilV2Controller* controller         = new PowerColorRedDevilV2Controller(bus, i2c_addr);
         RGBController_PowerColorRedDevilV2* rgb_controller = new RGBController_PowerColorRedDevilV2(controller);
 
         ResourceManager::get()->RegisterRGBController(rgb_controller);
-    //}
+    }
 }
 
 REGISTER_I2C_PCI_DETECTOR("PowerColor Red Devil RX9070XT", DetectPowerColorRedDevilGPUControllersV2, AMD_GPU_VEN, AMD_NAVI48_DEV, POWERCOLOR_SUB_VEN, POWERCOLOR_RED_DEVIL_RX9070XT_SUB_DEV, 0x22);
